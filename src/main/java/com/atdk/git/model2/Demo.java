5bd332c0ff6c17b6869eaa6aa6b873eacbc0eda5
张山 <zdk20001129@163.com>
2022-06-12 17:42:58 +0800

    model2 功能搭建开发

---
+++ src/main/java/com/atdk/git/model2/Demo.java
@@ -0,0 +1,13 @@
+package com.atdk.git.model2;
+
+/**
+ * @className: com.atdk.git.model2.Demo
+ * @description: TODO
+ * @author: 张山
+ * @create: 2022-06-12 17:41
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("main model2");
+    }
+}

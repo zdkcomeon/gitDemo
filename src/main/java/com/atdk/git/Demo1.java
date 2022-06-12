34e4ed3efac66520f736d10d7a5d0972ef7e9907
张山 <zdk20001129@163.com>
2022-06-12 17:30:19 +0800

    忽略我们的 tar

--- src/main/java/com/atdk/git/Demo1.java
+++ src/main/java/com/atdk/git/Demo1.java
@@ -1,8 +1,13 @@
-package com.atdk.git;/**
-@className: com.atdk.git.Demo1
-@description: TODO
-@author: 张山
-@create: 2022-06-12 17:24
-*/
+package com.atdk.git;
+
+/**
+ * @className: com.atdk.git.Demo1
+ * @description: TODO
+ * @author: 张山
+ * @create: 2022-06-12 17:24
+ */
 public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("程序跑起来了。。。。");
+    }
 }

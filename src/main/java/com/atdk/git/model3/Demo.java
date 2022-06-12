063d96b102a009778d195f06109ffa9bd2ab170f
张山 <zdk20001129@163.com>
2022-06-12 18:00:15 +0800

    model 3 第二天提交

--- src/main/java/com/atdk/git/model3/Demo.java
+++ src/main/java/com/atdk/git/model3/Demo.java
@@ -10,5 +10,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("model 3");
         System.out.println("model 3 第一天提交");
+        System.out.println("model 3 第二天提交");
     }
 }

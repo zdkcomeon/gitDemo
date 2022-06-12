8274d4948cba2935aa3d9c076590f73c5334544e
张山 <zdk20001129@163.com>
2022-06-12 17:36:17 +0800

    model1 模块提交

---
+++ src/main/java/com/atdk/git/model1/Demo.java
@@ -0,0 +1,13 @@
+package com.atdk.git.model1;
+
+/**
+ * @className: com.atdk.git.model1.Demo
+ * @description: TODO
+ * @author: 张山
+ * @create: 2022-06-12 17:33
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("model1");
+    }
+}

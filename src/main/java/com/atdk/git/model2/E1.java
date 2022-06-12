24754a57a0064db6f2a286492766b677f2e6123b
张山 <zdk20001129@163.com>
2022-06-12 17:44:01 +0800

    model2 第一天开发提交

---
+++ src/main/java/com/atdk/git/model2/E1.java
@@ -0,0 +1,13 @@
+package com.atdk.git.model2;
+
+/**
+ * @className: com.atdk.git.model2.E1
+ * @description: TODO
+ * @author: 张山
+ * @create: 2022-06-12 17:43
+ */
+public class E1 {
+    public static void main(String[] args) {
+        System.out.println("第一天开发");
+    }
+}

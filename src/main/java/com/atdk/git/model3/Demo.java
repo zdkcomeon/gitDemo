acb41c34c732c095e0acc9eeef9285fbc0c544c5
张山 <zdk20001129@163.com>
2022-06-12 17:59:14 +0800

    model 3 创建并提交的新的分支

---
+++ src/main/java/com/atdk/git/model3/Demo.java
@@ -0,0 +1,13 @@
+package com.atdk.git.model3;
+
+/**
+ * @className: com.atdk.git.model3.Demo
+ * @description: TODO
+ * @author: 张山
+ * @create: 2022-06-12 17:58
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("model 3");
+    }
+}
